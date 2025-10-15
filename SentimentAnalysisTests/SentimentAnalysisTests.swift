b10d3f726a0b6d3a0b286af6d3cc35f8a0ff419b
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-16 01:33:12 +0200

    Initial Commit

---
+++ SentimentAnalysisTests/SentimentAnalysisTests.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisTests.swift
+//  SentimentAnalysisTests
+//
+//  Created by Azizbek Asadov on 16.10.2025.
+//
+
+import Testing
+@testable import SentimentAnalysis
+
+struct SentimentAnalysisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
